d31afde1fe26d9506251f41c1de52f0e23833375
peterschellhorn <peterschellhorn@localhost>
2021-02-15 18:34:57 -0500

    Intro/review Assignment 1

---
+++ Assignment1/src/assignment1/Assignment1.java
@@ -0,0 +1,19 @@
+/*
+Peter Schellhorn 
+BCS 345
+Assignment 1
+*/
+package assignment1;
+
+public class Assignment1 {
+
+ 
+    public static void main(String[] args) {
+        
+       for(int i = 0; i<100;i++){//initialize for loop
+            if(i%2==0 )//check for even number
+                System.out.println("i = " + i);//print even numbers
+    }
+    }
+    
+}
